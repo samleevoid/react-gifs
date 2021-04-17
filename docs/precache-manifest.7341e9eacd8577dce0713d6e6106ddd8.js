be6388e93124c06e16927071eb584b1268773891
Sam Leevoid <samleevoid@gmail.com>
2021-04-17 09:15:00 +0200

    docs

---
+++ docs/precache-manifest.7341e9eacd8577dce0713d6e6106ddd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c2ac23f99a21246ff9318a3e8253c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0c57180b4af0399d651",
+    "url": "/static/css/main.5cebaa5e.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0c57180b4af0399d651",
+    "url": "/static/js/main.a2327be4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
